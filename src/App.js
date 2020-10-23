f3bc02f67c19c13f496b6636a6ecdaa48ee9a631
Dinara <ditiviola@gmail.com>
2020-10-24 02:13:58 +0300

    add style-components rebass

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from 'emotion-theming'
+import theme from '@rebass/preset'
+import Diary from './containers/Diary'
 
-function App() {
+function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+        <Diary/>
+    </ThemeProvider>
   );
 }
 
---
+++ src/containers/Diary.js
@@ -0,0 +1,17 @@
+import {
+  Box,
+  Card,
+  Image,
+  Heading,
+  Text
+} from 'rebass'
+
+function Diary() {
+	return (
+		<Box textAlign='center' mt='100px'>
+			<Text fontWeight='800' >Привет!</Text>
+		</Box>
+	)
+}
+
+export default Diary
