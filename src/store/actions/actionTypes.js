77437a870a56c1a139d43601de75d8921a517133
Dinara <ditiviola@gmail.com>
2021-02-23 00:17:50 +0300

    issue DIAR-10

--- src/store/actions/actionTypes.js
+++ src/store/actions/actionTypes.js
@@ -3,6 +3,8 @@ export const AUTH_SUCCESS = 'AUTH_SUCCESS';
 export const AUTH_FAIL = 'AUTH_FAIL';
 export const AUTH_LOGOUT = 'AUTH_LOGOUT';
 export const SET_REDIRECT_PATH = 'SET_REDIRECT_PATH';
+export const RE_AUTH_SUCCESS = 'RE_AUTH_SUCCESS';
+export const RE_AUTH_FAIL = 'RE_AUTH_FAIL';
 
 
 export const SET_ACTIVE = 'SET_ACTIVE';
