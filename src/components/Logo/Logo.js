78ae6757d5ff72ad0c79f2cefefd2db612d45d82
Dinara <ditiviola@gmail.com>
2021-06-27 23:04:50 +0300

    test1

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -6,6 +6,7 @@ import s from './Logo.module.css';
 const logo = (props) => (
     <div className={s.Logo} style={{height: props.height}}>
         <img src={burgerLogo}  style={{height: '53px'}} alt="Diary" />
+        {test1}
     </div>
 );
 
