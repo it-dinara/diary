1efb3dbb9ae85d9d4c4be767d94eb17628993d7e
Dinara Usenakunova <ditiviola@gmail.com>
2024-02-13 14:57:24 +0300

    added redux-toolkit package

---
+++ src/features/read/readSlice.js
@@ -0,0 +1,11 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [];
+
+const readSlice = createSlice({
+  name: "read",
+  initialState, 
+  readucers: {
+    
+  }
+});
--- src/index.js
+++ src/index.js
@@ -1,57 +1,33 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
 
-// ReactDOM.render(
-//   <React>
-//     <App />
-//   </React>,
-//   document.getElementById('root')
-// );
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import authReducer from "./store/reducers/auth";
+import diaryReducer from "./store/reducers/diary";
+import readReducer from "./store/reducers/read";
 
-// // If you want to start measuring performance in your app, pass a function
-// // to log results (for example: reportWebVitals(console.log))
-// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
-
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
-import thunk from 'redux-thunk';
-
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import authReducer from './store/reducers/auth';
-import diaryReducer from './store/reducers/diary';
-import readReducer from './store/reducers/read';
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const rootReducer = combineReducers({
-	auth: authReducer,
-	diary: diaryReducer,
-	read: readReducer
+const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    diary: diaryReducer,
+    read: readReducer,
+  },
 });
 
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
-
-
-
 const app = (
-    <Provider store={store}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </Provider>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
 
-ReactDOM.render( app, document.getElementById( 'root' ) );
+ReactDOM.render(app, document.getElementById("root"));
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
