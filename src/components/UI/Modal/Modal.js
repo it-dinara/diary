00d01c83761db9acb08e1f11098028b1878dc50d
Dinara Usenakunova <ditiviola@gmail.com>
2024-04-19 21:25:03 +0300

    rewrite Modal Component in  React.memo

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,33 +1,22 @@
-import React, { Component } from 'react';
+import React from "react";
+import classes from "./Modal.module.css";
+import Backdrop from "../Backdrop/Backdrop";
 
-import classes from './Modal.module.css';
-import Backdrop from '../Backdrop/Backdrop';
-
-class Modal extends Component {
-
-    shouldComponentUpdate ( nextProps, nextState ) {
-        return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
-    }
-
-    UNSAFE_componentWillUpdate () {
-        // console.log('[Modal] WillUpdate');
-    }
-
-    render () {
-        return (
-            <>
-                <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
-                <div
-                    className={classes.Modal}
-                    style={{
-                        transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                        opacity: this.props.show ? '1' : '0'
-                    }}>
-                    {this.props.children}
-                </div>
-            </>
-        )
-    }
-}
+const Modal = React.memo((props) => {
+  return (
+    <>
+      <Backdrop show={props.show} clicked={props.modalClosed} />
+      <div
+        className={classes.Modal}
+        style={{
+          transform: props.show ? "translateY(0)" : "translateY(-100vh)",
+          opacity: props.show ? "1" : "0",
+        }}
+      >
+        {props.children}
+      </div>
+    </>
+  );
+});
 
 export default Modal;
