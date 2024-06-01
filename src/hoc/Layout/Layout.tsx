706eeafd7d0f3832d47465b0c659942f40010a67
Dinara Usenakunova <ditiviola@gmail.com>
2024-06-01 16:03:03 +0300

    rewrote Layout to typescript

---
+++ src/hoc/Layout/Layout.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Aux from "../_Aux/_Aux";
+import classes from "./Layout.module.css";
+import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
+import SideDrawer from "../../components/Navigation/SideDrawer/SideDrawer";
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { authToken } from "../../features/authSlice";
+
+type TLayout = {
+  children: React.ReactNode;
+};
+
+export default function Layout(props: TLayout) {
+  const [showSideDrawer, setShowSideDrawer] = useState(false);
+  const isAuthentication = useSelector(authToken) !== null;
+  const sideDrawerClosedHandler = () => {
+    setShowSideDrawer(false);
+  };
+
+  const sideDrawerToggleHandler = () => {
+    setShowSideDrawer(!showSideDrawer);
+  };
+  return (
+    <Aux>
+      <Toolbar
+        drawerToggleClicked={sideDrawerToggleHandler}
+        isAuthentication={isAuthentication}
+      />
+      <SideDrawer
+        isAuthentication={isAuthentication}
+        open={showSideDrawer}
+        closed={sideDrawerClosedHandler}
+      />
+      <main className={classes.Content}>{props.children}</main>
+    </Aux>
+  );
+}
